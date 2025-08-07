7dd9582a5d3edc126476e9bf63a96f3aaff67cec
guayaquil <guayaquil@vbox.senacsp.edu.br>
2025-08-07 11:46:52 -0300

    Feature: Registrar Email no Database

--- src/app/processa_news.php
+++ src/app/processa_news.php
@@ -23,6 +23,24 @@
 
 $email = $_POST['email'];
 
-echo "<h2>{$email}</h2>";
+# echo "<h2>{$email}</h2>";
 
+# 1. Conectando ao Banco de Dados via PDO()
+$conexao = new PDO( "mysql:host=db;dbname=estetica_t99;port=3306","root","root" );
+
+# 2. Instrução SQL
+$sql_create_table = "CREATE TABLE IF NOT EXISTS estetica_t99.news ( 
+    id_news INT(255) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(254) NOT NULL UNIQUE,
+    validado INT(1) NOT NULL DEFAULT 0,
+    opt_out ENUM('sim','não') NOT NULL DEFAULT 'sim',
+    data_criacao DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+)";
+
+$sql_insert_email = "INSERT INTO estetica_t99.news (email)
+VALUES (?)";
+
+# 3. Executar o comando através da conexão
+$executar = $conexao -> prepare($sql_insert_email);
+$executar->execute([$email]);
 ?>
