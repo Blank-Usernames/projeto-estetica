26db7dbbb9ff09336af8305df605d566b6c7a1d3
guayaquil <guayaquil@vbox.senacsp.edu.br>
2025-08-21 10:03:00 -0300

    Feature: Front-End V1

--- application/resources/views/home.blade.php
+++ application/resources/views/home.blade.php
@@ -9,33 +9,43 @@
     <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
 
     <style>
+        * {
+            box-sizing: border-box;
+            max-width: 100%;
+        }
+
         img {
             width: 100%;
             max-width: 100%;
         }
+
+        .conteudos {
+            width: calc( (100% / 12 * 10) - 1rem);
+        }
     </style>
 </head>
 <body>
-    <main class=" bg-light py-4 px-3 d-flex flex-row" style="height: 100vh;">
-        <section class="col-2 navbar navbar-expand d-flex flex-column">
+    <main class="bg-primary p-3 d-flex flex-row" style="height: 100vh;">
+        <section class="col-2 navbar navbar-expand d-flex flex-column bg-light rounded-1">
+
             <!-- Navbrand -->
             <h1 class="container-fluid">
                 <a class="navbar-brand" href="{{config('app.url')}}">
                     <img src="https://placehold.co/1920x1080?text=Logo"  alt="Logotipo">
                 </a>
-                <button>
-                    <span class="navbar-toggler-icon"><i class="bi bi-list"></i></span>
+                <button class="btn btn-outline-light">
+                    <span class="navbar-toggler-icon"></i></span>
                 </button>
             </h1>
 
-            <nav>
-                <a href="{{config('app.url')}}"><i class="bi bi-speedometer2"></i>Painel</a>
-                <a href="#"><i class="bi bi-person-circle"></i>Usuários</a>
-                <a href="#"><i class="bi bi-newspaper"></i>Novidades</a>    
+            <nav class="w-100">
+                <a class="nav-link w-100 p-2 mb-2" href="{{config('app.url')}}"><span class="h4 ms-3 fw-normal"><i class="bi bi-speedometer2 pe-3"></span></i>Painel</a>
+                <a class="nav-link w-100 p-2 mb-2" href="{{config('app.url')}}"><span class="h4 ms-3 fw-normal"><i class="bi bi-person-circle pe-3"></span></i>Usuários</a>
+                <a class="nav-link w-100 p-2 mb-2" href="{{config('app.url')}}"><span class="h4 ms-3 fw-normal"><i class="bi bi-newspaper pe-3"></span></i>Novidades</a>   
             </nav>
         </section>
 
-        <section class="col-10">Conteúdos</section> 
+        <section class="col-10 bg-secondary ms-3 rounded-1 conteudos">Conteúdos</section> 
     </main>
 </body>
 </html>
